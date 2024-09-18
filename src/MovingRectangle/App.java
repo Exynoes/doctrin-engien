766e7773e9648ec3751002190e0c44cbec3774fe
Exynoes <158101082+Exynoes@users.noreply.github.com>
2024-09-18 14:08:42 -0400

    feat: amazing new feature

--- src/MovingRectangle/App.java
+++ src/MovingRectangle/App.java
@@ -1,6 +1,9 @@
 package MovingRectangle;
+
 import Doctrina.Game;
+
 public class App {
+
     public static void main(String[] args) {
         Game game = new MovingRectangleGame();
         game.start();
